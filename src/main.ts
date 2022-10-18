20e148534533ec76c59bdfb9e38a188aaea5c64a
Lucas L <lucas.lim@eblock.ca>
2022-10-18 09:14:06 -0400

    update action

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,57 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
+import axios from 'axios'
+import pWaitFor from 'p-wait-for'
 
 async function run(): Promise<void> {
+  const pipelineId = core.getInput('pipelineId', {required: true})
+  const token = core.getInput('token', {required: true})
+  const interval = Number(core.getInput('interval'))
+  const timeout = Number(core.getInput('timeout'))
+
+  core.debug(`pipelineId: ${pipelineId}`)
+  core.debug(`interval: ${interval}`)
+  core.debug(`timeout: ${timeout}`)
+
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    await pWaitFor(
+      async () => {
+        const response = await axios.get(
+          `https://circleci.com/api/v2/pipeline/${pipelineId}/workflow`,
+          {
+            headers: {
+              'x-attribution-login': github.context.actor,
+              'x-attribution-actor-id': github.context.actor,
+              'Circle-Token': token
+            }
+          }
+        )
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+        const status = response.data.items[0].status
 
-    core.setOutput('time', new Date().toTimeString())
+        if (status === 'running') {
+          return false
+        } else {
+          const circleciResponse = {
+            pipelineId: response.data.items[0].pipeline_id,
+            pipelineNumber: response.data.items[0].pipeline_number,
+            status: response.data.items[0].status
+          }
+          core.debug(`response: ${JSON.stringify(circleciResponse)}`)
+          core.setOutput('response', circleciResponse)
+
+          return true
+        }
+      },
+      {
+        interval,
+        timeout
+      }
+    )
   } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
+    if (error instanceof Error) {
+      core.setFailed(error.message)
+    }
   }
 }
 
